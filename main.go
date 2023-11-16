c75301dd76655f7c0b45b403c169b8266e9082a1
Renato Fagalde <renato@likwi.com.br>
2023-11-16 20:52:27 -0300

    🖥️🖥️🖥️🖥️🖥️  114

--- main.go
+++ main.go
@@ -16,7 +16,7 @@ func main() {
 
 func handler(ctx context.Context, req events.APIGatewayProxyRequest) (events.APIGatewayProxyResponse, error) {
 	seed := time.Now().UTC().UnixNano()
-	nameGenerator := fmt.Sprintf("Random pipe 113 -> %s", namegenerator.NewNameGenerator(seed).Generate())
+	nameGenerator := fmt.Sprintf("Random pipe 114 -> %s", namegenerator.NewNameGenerator(seed).Generate())
 
 	fmt.Println("console")
 
