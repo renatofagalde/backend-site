20cbeca8f2bc020b1e0efa70ab40e8e0b15e0f40
Renato Fagalde <renato@likwi.com.br>
2023-11-16 13:21:23 -0300

    set name of resources

--- main.go
+++ main.go
@@ -16,7 +16,7 @@ func main() {
 
 func handler(ctx context.Context, req events.APIGatewayProxyRequest) (events.APIGatewayProxyResponse, error) {
 	seed := time.Now().UTC().UnixNano()
-	nameGenerator := fmt.Sprintf("Random pipe 101 -> %s", namegenerator.NewNameGenerator(seed).Generate())
+	nameGenerator := fmt.Sprintf("Random pipe 102 -> %s", namegenerator.NewNameGenerator(seed).Generate())
 
 	fmt.Println("console")
 
