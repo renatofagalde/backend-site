4840819ddc3f5801c47c9c14f88402e0e7b24ba9
Renato Fagalde <renato@likwi.com.br>
2023-11-18 12:18:47 -0300

    log

--- src/controller/site/create_controller.go
+++ src/controller/site/create_controller.go
@@ -1,11 +1,13 @@
 package controller
 
 import (
+	"fmt"
 	"github.com/gin-gonic/gin"
 	"net/http"
 )
 
 func (site *siteControllerInterface) Create(c *gin.Context) {
 
+	fmt.Println("create")
 	c.JSON(http.StatusOK, "test-gin-adapter-ok")
 }
--- src/controller/site/find_by_id_controller.go
+++ src/controller/site/find_by_id_controller.go
@@ -1,11 +1,14 @@
 package controller
 
 import (
+	"fmt"
 	"github.com/gin-gonic/gin"
 	"net/http"
 )
 
 func (site *siteControllerInterface) FindById(c *gin.Context) {
+
+	fmt.Println("find by id")
 
 	c.JSON(http.StatusOK, "test-gin-adapter-ok")
 }
