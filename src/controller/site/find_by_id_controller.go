dbdbcc6fe00e7fab4919705af8e5858001cdf013
Renato Fagalde <renato@likwi.com.br>
2023-11-18 11:48:20 -0300

    findbyid

--- src/controller/site/find_by_id_controller.go
+++ src/controller/site/find_by_id_controller.go
@@ -1,7 +1,11 @@
 package controller
 
-import "github.com/gin-gonic/gin"
+import (
+	"github.com/gin-gonic/gin"
+	"net/http"
+)
 
 func FindById(c *gin.Context) {
 
+	c.JSON(http.StatusOK, "test-gin-adapter-ok")
 }
